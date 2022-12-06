116c4cd57e617f80c7acfa7a67c3e7baf0282bb5
Ludovic Alarcon <contact@ludovic-alarcon.com>
2022-12-06 21:03:14 +0100

    feat: handle multiple sources

--- main.go
+++ main.go
@@ -12,22 +12,35 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
+const sourcesPath = "sources.txt"
+const dbPath = "db.txt"
+
+func isSameDay(date1 time.Time, date2 time.Time) bool {
+	if date1.Year() == date2.Year() && date1.Month() == date2.Month() && date1.Day() == date2.Day() {
+		return true
+	}
+	return false
+}
+
 func clearDbForNewDay(firstLine string, filePath string, file *os.File) *os.File {
 	const dateLayout = "2006-01-02"
 
 	currentDate := time.Now().UTC()
-	dbDate, err := time.Parse(dateLayout, firstLine)
-	isNeededToClear := false
+	isNeededToClear := true
 
-	if err != nil {
-		log.Fatalln(err)
-	}
+	if firstLine != "" {
+		dbDate, err := time.Parse(dateLayout, firstLine)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	if currentDate.Year() != dbDate.Year() || currentDate.Month() != dbDate.Month() || currentDate.Day() != dbDate.Day() {
-		isNeededToClear = true
+		if isSameDay(currentDate, dbDate) {
+			isNeededToClear = false
+		}
 	}
 
 	if isNeededToClear {
+		log.Println("Clear db")
 		data := []byte(fmt.Sprintf("%v\n", currentDate.Format(dateLayout)))
 		err := os.WriteFile(filePath, data, 0644)
 		if err != nil {
@@ -42,7 +55,7 @@ func retrieveDbData(scanner *bufio.Scanner) map[string]string {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		dbData := strings.Fields(line)
+		dbData := strings.Split(line, "|-|")
 
 		if len(dbData) != 2 {
 			log.Println("Skiping data")
@@ -55,7 +68,7 @@ func retrieveDbData(scanner *bufio.Scanner) map[string]string {
 
 func initDbFile(filePath string) map[string]string {
 
-	file, err := os.Open(filePath)
+	file, err := os.OpenFile(filePath, os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -71,24 +84,52 @@ func initDbFile(filePath string) map[string]string {
 	return db
 }
 
-func retrieveFeeds(db map[string]string) map[string]string {
+func retrieveFeeds(db map[string]string, feedUrl string, currentDate time.Time) map[string]string {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURLWithContext("https://azure.microsoft.com/en-us/blog/feed/", ctx)
+	feed, err := fp.ParseURLWithContext(feedUrl, ctx)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	for _, item := range feed.Items {
-		db[item.GUID] = item.Title
-		fmt.Println(item.Link)
-		fmt.Println("-------------------")
+		if isSameDay(currentDate, item.PublishedParsed.UTC()) && db[item.GUID] == "" {
+			db[item.GUID] = item.Title
+		}
+	}
+	return db
+}
+
+func retrieveFeedsFromSources(db map[string]string) map[string]string {
+	currentDate := time.Now().UTC()
+
+	file, err := os.Open(sourcesPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		url := scanner.Text()
+		db = retrieveFeeds(db, url, currentDate)
 	}
 
 	return db
 }
 
+func send(db map[string]string) {
+	for key, value := range db {
+		fmt.Printf("%s %s\n", key, value)
+	}
+}
+
 func saveDb(db map[string]string) {
-	file, err := os.OpenFile("db.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(dbPath, os.O_APPEND|os.O_WRONLY, 0644)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -97,7 +138,7 @@ func saveDb(db map[string]string) {
 	defer file.Close()
 
 	for key, value := range db {
-		tmp := fmt.Sprintf("%s %s\n", key, value)
+		tmp := fmt.Sprintf("%s|-|%s\n", key, value)
 
 		_, err := file.WriteString(tmp)
 
@@ -109,12 +150,10 @@ func saveDb(db map[string]string) {
 
 func main() {
 
-	db := initDbFile("db.txt")
-	db = retrieveFeeds(db)
+	db := initDbFile(dbPath)
+	db = retrieveFeedsFromSources(db)
 
-	if db["123"] != "" {
-		fmt.Println("found")
-	}
+	send(db)
 
 	saveDb(db)
 }
